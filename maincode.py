8ca77a30cc64738b9b85b93a02d8ff90b9f81f90
Leo-Sharm <95899441+Leo-Sharm@users.noreply.github.com>
2023-04-18 19:19:20 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -10,13 +10,13 @@ def find_largest(num1, num2, num3):
 
 st.title("Find the Largest Number")
 
-# create three input fields for the user to enter numbers
-num1 = st.number_input("Enter the first number:")
-num2 = st.number_input("Enter the second number:")
-num3 = st.number_input("Enter the third number:")
+# create three mandatory input fields for the user to enter numbers
+num1 = st.number_input("Enter the first number:", value=0.0, step=None, required=True)
+num2 = st.number_input("Enter the second number:", value=0.0, step=None, required=True)
+num3 = st.number_input("Enter the third number:", value=0.0, step=None, required=True)
 
 # calculate the largest number
 largest = find_largest(num1, num2, num3)
 
-# display the largest number
-st.write("The largest number is:", largest)
+# display the largest number in a big font size
+st.write("The largest number is:", largest, "<span style='font-size:72px;'>" + str(largest) + "</span>", unsafe_allow_html=True)
