78cfd16b38e5635d0174eb04bf5804fec7b48473
Leo-Sharm <95899441+Leo-Sharm@users.noreply.github.com>
2023-04-18 19:28:55 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -11,12 +11,12 @@ def find_largest(num1, num2, num3):
 st.title("Find the Largest Number")
 
 # create three mandatory input fields for the user to enter numbers
-num1 = st.number_input("Enter the first number:", value=0.0, step=None, format='%f', key='num1')
-num2 = st.number_input("Enter the second number:", value=0.0, step=None, format='%f', key='num2')
-num3 = st.number_input("Enter the third number:", value=0.0, step=None, format='%f', key='num3')
+num1 = st.number_input("Enter the first number:", value=0.0, step=None, format='%f', key='num1', min_value=0.0)
+num2 = st.number_input("Enter the second number:", value=0.0, step=None, format='%f', key='num2', min_value=0.0)
+num3 = st.number_input("Enter the third number:", value=0.0, step=None, format='%f', key='num3', min_value=0.0)
 
 if st.button("Find the Largest Number"):
-    if num1 and num2 and num3:
+    if num1 is not None and num2 is not None and num3 is not None:
         # calculate the largest number
         largest = find_largest(num1, num2, num3)
 
